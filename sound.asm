db72abb75119088a2b8898fd84492f8e550dce80
jenishgg <jgurung2@student.ohlone.edu>
2024-12-04 01:03:45 -0800

    sound2

--- sound.asm
+++ sound.asm
@@ -50,19 +50,19 @@ li $v0 31 	# play midi sound
 syscall 	# do it
 
 li $v0, 32	# sleep
-li $a0, 300	# for 300 miliseconds
+li $a0, 400	# for 400 miliseconds
 syscall
 
 
 
 li $a0, 61	# play C#    5)
-li $a1 400 	# play for 400 miliseconds
+li $a1 400 	# play for 500 miliseconds
 
 li $v0 31 	# play midi sound
 syscall 	# do it
 
 li $v0, 32	# sleep
-li $a0, 200	# for 200 miliseconds
+li $a0, 400	# for 200 miliseconds
 syscall
 
 
@@ -74,19 +74,19 @@ li $v0 31 	# play midi sound
 syscall 	# do it
 
 li $v0, 32	# sleep
-li $a0, 200	# for 200 miliseconds
+li $a0, 300	# for 200 miliseconds
 syscall
 
 
 
 li $a0, 62	# play D    7)
-li $a1 500 	# play for 500 miliseconds
+li $a1 400 	# play for 500 miliseconds
 
 li $v0 31 	# play midi sound
 syscall 	# do it
 
 li $v0, 32	# sleep
-li $a0, 300	# for 300 miliseconds
+li $a0, 400	# for 300 miliseconds
 syscall
 
 
@@ -98,7 +98,7 @@ li $v0 31 	# play midi sound
 syscall 	# do it
 
 li $v0, 32	# sleep
-li $a0, 300	# for 300 miliseconds
+li $a0, 400	# for 300 miliseconds
 syscall
 
 
@@ -140,7 +140,7 @@ syscall
 
 
 li $a0, 64	# play E    12)
-li $a1 400 	# play for 400 miliseconds
+li $a1 300 	# play for 400 miliseconds
 
 li $v0 31 	# play midi sound
 syscall 	# do it
