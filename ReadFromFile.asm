5f449b99b331ed4f741a0c1973ffea0f019c9d65
Rameen Kayoumy <rkayoumy1@student.ohlone.edu>
2024-12-04 02:47:25 -0800

    fixed

--- ReadFromFile.asm
+++ ReadFromFile.asm
@@ -70,3 +70,85 @@ ending:
 	
 	lw $ra, 0($fp)
 	jr $ra
+	
+	
+	
+	
+	
+	
+# preconditions:
+#	$a0 = file path
+#	$a1 = buffer
+# postcondition:
+#	The buffer address hold the file text
+read:
+
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+
+	move $s0, $a0			# stash the file path
+	move $s1, $a1			# stash the buffer address
+	
+	li $v0, 13			# open the file
+	move $a0, $s0			# set the file path
+	li $a1, 0
+	li $a2, 0
+	syscall
+
+	move $s3, $v0			# save the file handler
+	
+	li $v0, 14			# read the file
+	move $a0, $s3			# set the file handler
+	move $a1, $s1			# set the buffer
+	li $a2, 199			# set the max length
+	syscall
+	
+	move $s4, $v0			# save the number of chars read
+	
+	add $s5, $s4, $s1
+	sb $zero, 0($s5)		# insert the terminating null char (\0)
+	
+	li $v0, 16			# close the file
+	move $a0, $s3			# set the file handler
+	syscall
+	
+	lw $ra, 0($sp)
+	
+	jr $ra
+	
+	
+#converts a string of ascii character numbers into 
+#preconditions:
+#	$a0 coutains the address of the null-terminated string
+convertToInt:
+	addi $sp, $sp, -24
+	li $s7, 0
+	li $s5, 13         
+	move $s1, $a0
+	li $s3, 10
+	li $s2, 0
+loop:
+  lbu $s4, ($s1)       #load unsigned char from array into t1
+  beq $s4, $s5, oneDone
+  beq $s4, $zero, FIN     #NULL terminator found
+  addi $s4, $s4, -48   #converts t1's ascii value to dec value
+  mul $s2, $s2, $s3    #sum *= 10
+  add $s2, $s2, $s4    #sum += array[s1]-'0'
+  addi $s1, $s1, 1     #increment array address
+  j loop       #jump to start of loop
+FIN:
+	lw $t1, -24($sp)
+	lw $t2, -20($sp)
+	lw $t3, -16($sp)
+	lw $t4, -12($sp)
+	lw $t5, -8($sp)
+	lw $t6, -4($sp)
+	jr $ra
+oneDone:
+  	#add $sp, $sp, $s7
+	sw $s2, 0($sp)
+	li $s2, 0
+	#addi $s7, $s7, 4
+	addi $sp, $sp, 4
+	addi $s1, $s1, 2     #increment array address
+	j loop
